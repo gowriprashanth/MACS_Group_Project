1a3e9a9f9f553397c7fe8acbeb4c1dabd89084dd
ramandeep <rm661081@dal.ca>
2023-07-03 20:00:54 -0300

    Added posts details page

--- accomatch/src/main/webapp/frontend/src/App.js
+++ accomatch/src/main/webapp/frontend/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import NavBar from "./Components/NavBar";
-import { Home } from "./Components/Pages/Home";
-import { Posts } from "./Components/Pages/Posts";
-import { Applicants } from "./Components/Pages/Applicants";
-import { Contact } from "./Components/Pages/Contact";
- import {useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import { Home } from './Components/Pages/Home';
+import { Posts } from './Components/Pages/Posts';
+import { PostsDetailsPage } from './Components/Pages/PostsDetailsPage';
+import { Applicants } from './Components/Pages/Applicants';
+import { Contact } from './Components/Pages/Contact';
 import { Login } from './Login';
 import { Signup } from './Signup';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
@@ -18,22 +18,14 @@ function App() {
 
   return (
     <Router>
-      <main className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={<Login onLoginSuccess={handleLoginSuccess} />}
-          />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </main>
-
-      {<NavBar />}
-
+      {isLoggedIn && <NavBar />}
       <div className="pages">
         <Routes>
+          <Route path="/" element={<Login onLoginSuccess={handleLoginSuccess} />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
           <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/:applicationId" element={<PostsDetailsPage />} />
           <Route path="/applicants" element={<Applicants />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
@@ -41,6 +33,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
