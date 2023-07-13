ce3a2d739725bb88454602bbe34f5b97361739ad
Yogish_hg <hg15yogish@gmail.com>
2023-07-13 17:49:43 -0300

    basic impl

--- accomatch/src/main/java/com/project/accomatch/Controller/AdminController.java
+++ accomatch/src/main/java/com/project/accomatch/Controller/AdminController.java
@@ -1,4 +1,12 @@
 package com.project.accomatch.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
 }
