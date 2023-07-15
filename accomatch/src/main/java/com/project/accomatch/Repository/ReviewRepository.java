764dacd1b5f398052874c70f0c0cf01c1bf548fd
DHRUMIL VIMALBHAI GOSALIYA <dh411197@dal.ca>
2023-07-15 20:16:24 -0300

    createreview api

--- accomatch/src/main/java/com/project/accomatch/Repository/ReviewRepository.java
+++ accomatch/src/main/java/com/project/accomatch/Repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.project.accomatch.Repository;
 
+import com.project.accomatch.Model.Review;
 import com.project.accomatch.Model.UserModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -20,7 +21,32 @@ public class ReviewRepository {
     @Value("${Connection.db.accomatch}")
     private String JDBC;
 
+    public int createReview(Review review) {
+        try {
+            Connection connect;
+            Statement statement;
+            // Connect to the database.
+            //getCredentials();
+            connect = DriverManager.getConnection(JDBC, username, password);
+            // Create a statement object.
+            statement = connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
+            String sql = "INSERT INTO rating_table (user_id,application_id,rating,review)" +
+                    "VALUES (?,?,?,?)";
+            PreparedStatement stmt = connect.prepareStatement(sql);
+            stmt.setInt(1, review.getUserId());
+            stmt.setInt(2, review.getApplicationId());
+            stmt.setInt(3, review.getRating());
+            stmt.setString(4, review.getComment());
+
+            int rs = stmt.executeUpdate();
+            stmt.close();
+            connect.close();
+            return rs;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
--- accomatch/src/main/java/com/project/accomatch/Service/Implementation/ReviewServiceImplementation.java
+++ accomatch/src/main/java/com/project/accomatch/Service/Implementation/ReviewServiceImplementation.java
@@ -3,27 +3,38 @@ package com.project.accomatch.Service.Implementation;
 import com.project.accomatch.Model.Review;
 import com.project.accomatch.Repository.ReviewRepository;
 import com.project.accomatch.Service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ReviewServiceImplementation implements ReviewService {
-    private final ReviewRepository reviewRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
 
-    public ReviewServiceImplementation(ReviewRepository reviewRepository) {
-        this.reviewRepository = reviewRepository;
-    }
+    @Value("${username.db.accomatch}")
+    private String username;
+
+    @Value("${password.db.accomatch}")
+    private String password;
+
+    @Value("${Connection.db.accomatch}")
+    private String JDBC;
 
     @Override
-    public Review createReview(Review review) {
-        return reviewRepository.save(review);
+    public void createReview(Review review) {
+        try {
+            reviewRepository.createReview(review);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<Review> getAllReviews() {
-        return reviewRepository.findAll();
+        return null;
     }
 
-    // Implement other methods for retrieving, updating, deleting reviews
 }
