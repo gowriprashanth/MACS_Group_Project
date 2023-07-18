fcb267fcf32d80196b3f62e67d36d9076061b00b
Gowri Prashanth Kanagaraj <gw351441@dal.ca>
2023-07-17 23:26:02 -0300

    average ratings api

--- accomatch/src/main/java/com/project/accomatch/Controller/PostRatingsViewController.java
+++ accomatch/src/main/java/com/project/accomatch/Controller/PostRatingsViewController.java
@@ -1,9 +1,11 @@
 package com.project.accomatch.Controller;
+import com.project.accomatch.Model.Ratings;
 import com.project.accomatch.Model.Review;
 import com.project.accomatch.Service.Implementation.ReviewServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,5 +17,13 @@ public class PostRatingsViewController {
     @GetMapping("/getListOfAllRatings/{application_id}")
     public List<Review> getListOfAllRatings(@PathVariable int application_id){
         return reviewServiceImplementation.getAllReviews(application_id);
+    }
+    @GetMapping("/getAverageRatings/{application_id}")
+    public List<Ratings> getAverageRatings(@PathVariable int application_id){
+        return reviewServiceImplementation.getRatingsAverage(application_id);
+        /*Ratings ratings = reviewServiceImplementation.getRatingsAverage(application_id);
+        List<Ratings> listOfReviews = new ArrayList<>();
+        listOfReviews.add(ratings);
+        return  listOfReviews;*/
     }
 }
