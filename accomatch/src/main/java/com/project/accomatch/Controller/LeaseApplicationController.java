c5b86a5652836d5b9cdbb1edc40901e2abb56c01
Gowri Prashanth Kanagaraj <gw351441@dal.ca>
2023-07-22 17:23:02 -0300

    added loggers

--- accomatch/src/main/java/com/project/accomatch/Controller/LeaseApplicationController.java
+++ accomatch/src/main/java/com/project/accomatch/Controller/LeaseApplicationController.java
@@ -1,8 +1,10 @@
 package com.project.accomatch.Controller;
 import com.project.accomatch.Exception.InvalidInputException;
+import com.project.accomatch.LoggerPack.LoggerClass;
 import com.project.accomatch.Model.Applicant;
 import com.project.accomatch.Service.Implementation.LeaseApplicationImplementation;
 import com.project.accomatch.Service.LeaseApplicationService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +16,13 @@ public class LeaseApplicationController {
     @Autowired
     private LeaseApplicationService applicantService;
 
+    Logger logger = LoggerClass.getLogger();
     @GetMapping("/get/list/applicant/{application_id}")
     public List<Applicant> getListOfPosts(@PathVariable int application_id) {
         if (application_id <= 0) {
             throw new InvalidInputException("Invalid application ID provided.");
         }
+        logger.info("Lease Applicant controller active");
         return applicantService.getListOfApplicants(application_id);
     }
 
